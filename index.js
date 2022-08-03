62892c5fa578af516cf263972eb61d894936915c
David Cabeza <cabezadavide@gmail.com>
2022-08-03 22:14:03 +0200

    Adds implementation to create and retrieve comments

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/posts/:id/comments', (req, res) => {
+const commentsByPostId = {};
 
+app.get('/posts/:id/comments', (req, res) => {
+  const postId = req.params.id;
+  res.send(commentsByPostId[postId] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({
+    id: commentId,
+    content,
+  });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
